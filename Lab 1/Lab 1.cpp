60971f67a8999b5e4a8836b7ec9457cf6fe7c61c
Michael <michaelkmwaura@gmail.com>
2023-01-17 17:24:36 -0500

    initialize repo

--- Lab 1/Lab 1.cpp
+++ Lab 1/Lab 1.cpp
@@ -1,20 +1,31 @@
-// Lab 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    /* Type code here. */
+    /* Overall flow of the program:
+    Get some input for the title
+    Get some input for column header #1
+    Get some input for column header #2
+
+    Until the user types "-1":
+    Get input for a string
+    Validate input
+    If valid...
+       Split
+       Validate second half of input
+       If also valid...
+          Store separate pieces
+
+    Print out data in table format
+
+    Print out data in chart format
+    */
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
