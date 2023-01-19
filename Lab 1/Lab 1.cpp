6e12d25a2b200e884923ddd0d1b8e07053d95a27
Michael <michaelkmwaura@gmail.com>
2023-01-19 13:48:57 -0500

    working on try/catch blocks

--- Lab 1/Lab 1.cpp
+++ Lab 1/Lab 1.cpp
@@ -26,7 +26,7 @@ int main()
 
     Print out data in chart format
     */
-    string title;
+  string title;
 	string col1;
 	string col2;
 	string input;
@@ -48,8 +48,63 @@ int main()
 	cout << "You entered: " << col2 << endl;
     
     while (input != "-1") {
-		cout << "Enter a data point (-1 to stop input): " << endl;
-		getline(cin, input);
+      int i = 0;
+            
+		  cout << "Enter a data point (-1 to stop input): " << endl;
+		  getline(cin, input);
+      
+      // checks if the input is valid using try/catch blocks
+      // if the entry has no comma
+      try {
+        if (input.find(",") == -1) {
+          throw 1;
+        }
+      }
+      catch (int e) {
+        cout << "Error: No comma in string." << endl;
+        continue;
+      }
+
+      // if the entry has more than one comma
+      try {
+        if (input.find(",") != input.rfind(",")) {
+          throw 2;
+        }
+      }
+      catch (int e) {
+        cout << "Error: Too many commas in input." << endl;
+        continue;
+      }
+
+      // if entry after the comma is not an integer using stoi
+      try {
+        if (input.find(",") != -1) {
+          stoi(input.substr(input.find(",") + 1, input.length()));
+        }
+      }
+      catch (invalid_argument e) {
+        cout << "Error: Comma not followed by an integer." << endl;
+        continue;
+      }
+
+      // default case
+      int commaIndex = input.find(",");
+      first = input.substr(0, commaIndex);
+      second = input.substr(commaIndex + 1, input.length());
+
+      // prints out the data points
+      cout << "Data string: " << first << endl;
+      cout << "Data integer: " << second << endl;
+
+      // appends the first and second strings to the vectors
+      firstVector.push_back(first);
+      secondVector.push_back(second);
+
+      if (input == "-1") {
+        break;
+      }
+      
+
         
 		
         
